7a63249e46fefbb80f22b39c820b52b6b6c88fff
Tudders <mark@tudders.com>
2013-11-14 10:52:08 +0000

    Adding file paths for folders

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -1,2 +1,8 @@
-import mob
-import gameStats
+import sys
+# Add the folder path to the sys.path list
+sys.path.append('/mob/')
+sys.path.append('/tower/')
+sys.path.append('/game stats/')
+sys.path.append('/map/')
+from mob import Mob
+from 'game stats' import GameStats
