e95c0075435da6e88417176b0d2aae704c0f9a0a
Arx-Dei <mark@tudders.com>
2013-11-14 17:54:46 +0000

    game stats class includes timer

--- SourceCode/game stats/gameStats.py
+++ SourceCode/game stats/gameStats.py
@@ -1,43 +1,44 @@
 import time
 class GameStats():
-    # Function that is run initialy setting an mount of lives and cash
+    # Function that is run initialy setting variables
     def __init__(self):
         self.health = 5
         self.cash = 100
         self.gameMode = 1
         self.currentTime = 0
         self.startTime = time.time()
+        self.error = ''
 
     def gameMode(self):
-        while True:
-            self.gameMode=int(input("Set gamemode to 1 or 2"))
-            if self.gameMode!=2 or self.gameMode!=1:
-                print("Gamemode must be set to 1 or 2")
-                continue
-            if self.gameMode == 1:
-                self.setHealth(5)
-                self.setCash(100)
-            else:
-                self.setHealth(100)
-                self.setCash(300000)
-            return self.gameMode
-            
+        if self.gameMode!=2 or self.gameMode!=1:
+            self.error = "Gamemode must be set to 1 or 2"
+            return 0
+        if self.gameMode == 1:
+            self.setHealth(5)
+            self.setCash(100)
+        else:
+            self.setHealth(100)
+            self.setCash(300000)
+        return 1
+        
     def getCash(self):
         return self.cash
-    
     def setCash(self, newCash):
         self.cash = newCash
         
     def getHealth(self):
         return self.health
-    
     def setHealth(self, newHealth):
         self.health = newHealth
-        
 
-    def CurrentTime():
-        CurrentTime=(time.time())
+    def getError(self):
+        return self.error
+
+    def currentTime():
+        currentTime=time.time() - self.startTime
         return CurrentTime
+
+    
 #ALL BELOW NEED TO BE IN A DEBUGGING FUNCTION IN FUTURE
 
 
