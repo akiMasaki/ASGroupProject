baeed499db7b3b16321c372ec89160299b9fb7d7
Harry Fryer <harryfryer@me.com>
2013-11-14 13:44:09 +0000

    Update main.py

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -4,6 +4,6 @@ sys.path.append('/mob/')
 sys.path.append('/tower/')
 sys.path.append('/game stats/')
 sys.path.append('/map/')
-from mob import Mob
-from 'game stats' import GameStats
+from mob import mob_dwarf
+from 'game stats' import gameStats
 from 'towers' import towers
