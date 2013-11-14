cf4e14f05f016b9591caa454df440830bf24535d
hfryer <HarryFryer@Harrys-MacBook-Air.local>
2013-11-14 21:25:25 +0000

    Basic Map Generation

--- SourceCode/map/map.py
+++ SourceCode/map/map.py
@@ -1,1 +1,43 @@
 #Map Files
+class Map () :
+    def __init__ (self):
+        self.map=[]
+
+    def setMap (self):
+        newMap=[]
+        for n in range (0,10):
+            newMap.append([])
+        for c in newMap :
+            for n in range(0,10):
+                c.append(0)
+        self.map=newMap
+    def returnMap (self):
+        return self.map
+
+
+
+Test1=Map()
+Test1.setMap()
+print(Test1.map)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
