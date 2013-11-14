ba55f964dbdf9beb8d4c9a331e5eb5fef275f15e
Tudders <mark@tudders.com>
2013-11-14 09:35:43 +0000

    Update mob.py

--- SourceCode/mob.py
+++ SourceCode/mob.py
@@ -1,1 +1,31 @@
 class Mob():
+  def __init__():
+    self.damage = 0
+    self.speed = 0
+    self.health = 0
+    self.damageToBase = 0
+  
+  
+  
+  
+  
+  
+  def setDamage():
+    return self.damage
+  def getDamage():
+    return self.damage
+    
+    def setSpeed():
+    return self.speed
+  def getSpeed():
+    return self.speed
+    
+    def setHealth():
+    return self.health
+  def getHealth():
+    return self.health
+    
+    def setDamageToBase():
+    return self.DamageToBase
+  def getDamageToBase():
+    return self.DamageToBase
