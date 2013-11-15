39c46495e75bd71cc99cd8526526fb91e8e1b566
Tudders <mark@tudders.com>
2013-11-15 08:43:10 +0000

    Update main.py

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -4,21 +4,56 @@ sys.path.append('/mob/')
 sys.path.append('/tower/')
 sys.path.append('/game stats/')
 sys.path.append('/map/')
+<<<<<<< HEAD
+#from mob import mob_dwarf
+=======
 from mob import *
+from mob_dwarf import Mob_Dwarf
 from gameStats import gameStats
+>>>>>>> a2816a57db893876738b76b3cae7dbacfc397d7e
 #from tower import towers
+from gameStats import gameStats
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 stats = gameStats.GameStats()
-dwarf = mob_dwarf.Mob_Dwarf()
+#dwarf = mob_dwarf.Mob_Dwarf()
 
 
 
+=======
+stats = gameStats.GameStats
+dwarf = mob_dwarf.Mob_Dwarf
+>>>>>>> a2816a57db893876738b76b3cae7dbacfc397d7e
+=======
+stats = gameStats.GameStats
+dwarf = mob_dwarf.Mob_Dwarf
+>>>>>>> a34f9a5acaf1fdc764db98e4173a037832b93e32
 
-print(dwarf.getHealth())
+#not working atm
+while True:
+    newGameMode = input('Enter Game Mode: ')
+    try:
+        newGameMode = int(newGameMode)
+    except ValueError:
+        print('Not a valid Number')
+        continue
+    
+    if stats.setGameMode(newGameMode):
+        print(stats.getError())
+    else:
+        break
+    
 
 
-print(stats.getError())
-if stats.setGameMode(3):
-    pass
-print(stats.getError())
+GameStats = GameStats()
 
+print(GameStats.getCash())
+
+GameStats.setCash(300000)
+print(GameStats.getCash())
+
+print(GameStats.getHealth())
+
+GameStats.setHealth(100)
+print(GameStats.getHealth())
