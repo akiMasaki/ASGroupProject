eb5063b48b6a7f6d783e8b897f0160c53f72f1b1
Arx-Dei <mark@tudders.com>
2013-11-16 23:45:10 +0000

    updated pygame main file

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -8,31 +8,35 @@ sys.path.append('/map/')
 from mob import mob
 from gameStats import gameStats
 
-stats = gameStats.GameStats()
-dwarf = mob.Mob_Dwarf()
-
 def main():
 # SETTING UP THE GAME AREA
     pygame.init()	
-    screen = pygame.display.set_mode( (720,670) )
-    background = pygame.Surface( screen.get_size() )
-    background.fill(142,206,250)
+    screen = pygame.display.set_mode((720,670))
+    
+    background = pygame.Surface(screen.get_size())
+    background.fill((142,206,250))
+    
     titleImage = pygame.image.load( "images/title.jpeg" )
     titleImage_position = image.get_rect()
     titleImage_position.bottom = 240
     titleImage_position.left = 200
+    
+    defaultFont = pygame.font.Font( None, 48 )
+    title = defaultFont.render('CLFS Tower Defence Game', 0, (0,0,0))
+
     screen.blit(background,(0,0))
     screen.blit(image, titleImage_position)
+    screen.blit( string, (100,100) )
     pygame.display.flip()
 
 # MAIN GAME LOOP
     while True:
         pygame.event.pump()
         keyinput = pygame.key.get_pressed()
-        if keyinput[pygame.K_ESCAPE] or pygame.event.peek( pygame.QUIT ):
+        if keyinput[pygame.K_ESCAPE] or pygame.event.peek(pygame.QUIT):
             break
-        screen.blit( background, (0,0) )
-        screen.blit( image, titleImage_position )
+        screen.blit(background, (0,0))
+        screen.blit(image, titleImage_position)
         pygame.display.flip()
 
 if __name__ == '__main__': main()
@@ -40,6 +44,9 @@ if __name__ == '__main__': main()
 
 """
 #FUNCTION TESTING
+
+stats = gameStats.GameStats()
+dwarf = mob.Mob_Dwarf()
 
 while True:
     newGameMode = input('Enter Game Mode: ')
