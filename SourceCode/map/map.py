0094fe1aa2e9e6a3745cdb5f37e51ffd08f00ebb
Harry Fryer <harryfryer@me.com>
2013-11-16 20:37:50 +0000

    Update map.py

--- SourceCode/map/map.py
+++ SourceCode/map/map.py
@@ -1,18 +1,30 @@
 #Map Files
 class Map () :
     def __init__ (self):
-        self.map=[]
+        self.currentMap=[]
 
-    def setMap (self):
+    def newMap (self):
         newMap=[]
         for n in range (0,10):
             newMap.append([])
         for c in newMap :
-            for n in range(0,10):
+            for n in range(0,5):
                 c.append(0)
-        self.map=newMap
+        self.currentMap=newMap
     def returnMap (self):
-        return self.map
+        return self.currentMap
+        
+    def updateMap (self,Cordintes,ChangeValue):
+        #cordinates get changed to location in array
+        split=list(str(Cordintes))
+        verticalCordinate=ord(split[0])-64
+        horizontalCoridinate=int(split[1])
+        #Updating the map instance to the new value
+        mapUpdateP1=self.currentMap
+        mapUpdateP2=mapUpdateP1[VerticalCordinate]
+        MapupdateP2[horizontalCordiante]=ChangeValue
+        
+        
 
 
 
