b4836721f88b96ffb9afd41b0b89bbe95ad4fe3c
hfryer <HarryFryer@Harrys-MacBook-Air.local>
2013-11-17 00:16:10 +0000

    Working on a Example Pathing Route

--- SourceCode/map/mapTestingForPathing.py
+++ SourceCode/map/mapTestingForPathing.py
@@ -3,6 +3,7 @@
 class Map () :
     def __init__ (self):
         self.currentMap=[]
+        self.path=[]
 
     def newMap (self):
         newMap=[]
@@ -11,19 +12,110 @@ class Map () :
         for c in newMap :
             for n in range(0,5):
                 c.append(0)
+
+        Starter=newMap[0]
+        Ender=newMap[9]
+        #THIS SLOT SHOULD NOT BE CHANGED
+        Starter[2]=2
+        Ender[2]=3
         self.currentMap=newMap
+        
     def getMap (self):
         return self.currentMap
         
     def updateMap (self,coordinates,changeValue):
+        
         #cordinates get changed to location in array
+        
         split=list(str(coordinates))
         verticalCoordinate=ord(split[0])-65
         horizontalCoordinate=int(split[1])-1
+        
         #Updating the map instance to the new value
+        
         mapUpdateP1=self.currentMap
         mapUpdateP2=mapUpdateP1[verticalCordinate]
         mapUpdateP2[horizontalCoridinate]=ChangeValue
+
+    def ReturnMap (self, coorinates):
+        
+        verticalCoordinate=coorinates[0]
+        horizontalCoordinate=coorinates[1]
+
+        mapReadP1=self.currentMap
+        mapReadP2=mapReadP1[verticalCoordinate]
+        Value=mapReadP2[horizontalCoordinate]
+        return Value
+        
+
+    def printMap (self):
+        print (self.currentMap)
+
+
+    def returnPath(self):
+        currentMap=self.currentMap
+        currentCooridnate=[0,2]
+        self.path=[[0,2]]
+        x=True
+
+        while x==True :
+            while True:
+                loopCheck=True
+                currentCooridnate[0]=currentCooridnate[0]+1
+                
+                if self.ReturnMap(currentCooridnate)==3:
+                    x=False
+                    break
+                
+                if self.ReturnMap(currentCooridnate)==2 or self.ReturnMap(currentCooridnate)==0:
+                    self.path.append(currentCooridnate)
+                    break
+
+                currentCooridnate[0]=currentCooridnate[0]-1
+                currentCooridnate[1]=currentCooridnate[1]+1
+
+                if Map.ReturnMap(currentCooridnate)==3:
+                    x=False
+                    break
+                
+                if self.ReturnMap(currentCooridnate)==2 or self.ReturnMap(currentCooridnate)==0:
+                    self.path.append(currentCooridnate)
+                    break
+
+                currentCooridnate[1]=currentCooridnate[1]-2
+
+                if self.ReturnMap(currentCooridnate)==3:
+                    x=False
+                    break
+                
+                if self.ReturnMap(currentCooridnate)==2 or self.ReturnMap(currentCooridnate)==0:
+                    self.path.append(currentCooridnate)
+                    break
+                
+                
+                currentCooridnate[0]=currentCooridnate[0]-1
+                currentCooridnate[1]=currentCooridnate[1]+2
+
+                if ReturnMap(currentCooridnate)==3:
+                    x=False
+                    break
+                
+                if self.ReturnMap(currentCooridnate)==2 or self.ReturnMap(currentCooridnate)==0:
+                    self.path.append(currentCooridnate)
+                    break
+        return self.path
+            
+                    
+            
+        
+
+TestMap=Map()
+
+TestMap.newMap()
+
+TestMap.printMap()
+
+print(TestMap.returnPath())
 
 
 
