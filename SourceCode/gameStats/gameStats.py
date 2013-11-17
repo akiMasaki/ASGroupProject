7d6f01d57dbe4447d9e23e2ac6cd1dfa8d0d8679
WamSilkinson <sam-wilkinson@hotmail.co.uk>
2013-11-17 16:08:16 +0000

    Update gameStats.py

--- SourceCode/gameStats/gameStats.py
+++ SourceCode/gameStats/gameStats.py
@@ -61,7 +61,6 @@ class GameStats():
     def mobKilled(self, mobType):
             self.cashDrop(self.killCash[mobType])
             if mobType == 'Dwarf':
-                cashDrop(30)
                 self.mobDwarfKills = self.mobDwarfKills + 1
 
     def cashDrop(self, cashToAdd):
