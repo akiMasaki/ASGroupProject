4f3ebee87d2e12cb9033d476c8400a3d4bb9b21a
Arx-Dei <mark@tudders.com>
2013-11-18 19:23:43 +0000

    Added Play button to title screen

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -13,26 +13,48 @@ import gameStats
 from pygame.locals import * # for getting mouse pos
     
 def playTitle():
+    title1 = defaultFont.render('CLFS', 0, (0,0,0))
+    title2 = defaultFont.render('Tower Defence', 0, (0,0,0))
+    playText = defaultFont.render('Play', 0, (0,0,0))
+    exitText = defaultFont.render('Exit', 0, (0,0,0))
+    
+    titleImage = pygame.image.load('images/title_image.jpg')
+    titleImage_position = titleImage.get_rect()
+    
     screen.blit(titleImage, titleImage_position)
     screen.blit(title1, (155,50))
     screen.blit(title2, (50,100))
+    screen.blit(playText, (285,363))
+    screen.blit(exitText, (285,451))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         elif event.type == MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
-        
+            mousePos = pygame.mouse.get_pos()
+            if (mousePos[0] >= 257 and  mousePos[0] <= 428) and (mousePos[1] >= 348 and  mousePos[1] <= 421):
+                gameStats.setLevel('MapSelect')
+            if (mousePos[0] >= 257 and  mousePos[0] <= 428) and (mousePos[1] >= 436 and  mousePos[1] <= 509):
+                pygame.quit()
+            
     pygame.display.flip()
 
-def playLevel1():
-    screen.blit(titleImage, titleImage_position)
+def playMapSelect():
     background = pygame.Surface(screen.get_size())
     background.fill((140,206,250))
     screen.blit(background, (0,0))
-    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == MOUSEBUTTONDOWN:
+            mousePos = pygame.mouse.get_pos()
     
-levelSelect = {'Title':playTitle, 'Level1':playLevel1}
+    pygame.display.flip()
+
+#MAIN 
+    
+levelSelect = {'Title':playTitle, 'MapSelect':playMapSelect}
 
 gameStats = gameStats.GameStats()
 gameStats.setLevel('Title')
@@ -40,13 +62,8 @@ gameStats.setLevel('Title')
 pygame.init()
 screen = pygame.display.set_mode((720,670))
 pygame.display.set_caption('CLFS Tower Defence')
-
-titleImage = pygame.image.load('images/title_image.jpg')
-titleImage_position = titleImage.get_rect()
-
 defaultFont = pygame.font.Font('fonts/miriamfixed.ttf', 48 )
-title1 = defaultFont.render('CLFS', 0, (0,0,0))
-title2 = defaultFont.render('Tower Defence', 0, (0,0,0))
+
 
 # MAIN GAME LOOP
 while gameStats.getLevel()!=None:
