a9babfa2b4e11685129726a0146327f4dc460eb0
akiMasaki <masaakiumemoto@yahoo.com>
2013-11-20 10:27:41 +0000

    Update towers.py

--- SourceCode/tower/towers.py
+++ SourceCode/tower/towers.py
@@ -4,22 +4,22 @@ import map_object
 
 class Tower(map_object.Map_Object):
     def _init_(self):
-        self.damage = 0
-        self.range = 0
+        self.dmg = 0
+        self.rng = 0
         self.numbUpdates{'Damage':0,'Range':0}
 
 #GET AND SET METHODS   
     def setDamage(self, nDamage):
-        self.damage = nDamage
+        self.dmg = nDamage
     def getDamage(self):
-        return self.damage
+        return self.dmg
 
     def setRange(self, nRange):
-        self.range = nRange
+        self.rng = nRange
     def getRange(self):
-        return self.range
+        return self.rng
 
     def upgradeRange(self):
-        self.range = self.range*1.5
-    def upgradeDamgae(self):
-        self.damage = self.damage*1.25
+        self.rng = self.rng*1.5
+    def upgradeDamage(self):
+        self.dmg = self.dmg*1.25
